85863af8339182e1163e0acf6ce821e95901cc0e
MrVikashMaurya <51211327+MrVikashMaurya@users.noreply.github.com>
2019-06-15 23:19:43 +0530

    matrix substraction
    matrix substraction of 3d aray

---
+++ MATRIX substraction.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+	int main()
+	{
+		int a[3][3],b[3][3],c[3][3],i,j,k;
+	clrscr();
+		printf("Enter your first Array\n");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				scanf("%d",&a[i][j]);
+			}
+		}
+			printf("Enter your second Array\n");
+			for(i=0;i<3;i++)
+			{
+				for(j=0;j<3;j++)
+				{
+					scanf("%d",&b[i][j]);
+				}
+			}
+	clrscr();
+		printf("After inputting your first matrix\n");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				printf("%d\t",a[i][j]);
+			}
+			printf("\n");
+		}
+			printf("\t-\n");
+
+		printf("After inputting your second matrix\n");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				printf("%d\t",b[i][j]);
+			}
+				printf("\n");
+		}
+			printf("\t=\n");
+			printf("Here is your matrix addition\n");
+			{
+				for(i=0;i<3;i++)
+				{
+					for(j=0;j<3;j++)
+					{
+						c[i][j]=a[i][j]-b[i][j];
+					}
+				}
+			}
+				printf("Here is your answer\n");
+				for(i=0;i<3;i++)
+				{
+					for(j=0;j<3;j++)
+					{
+						printf("%d\t",c[i][j]);
+					}
+						printf("\n");
+				}
+			getch();
+	return 0;
+	}
